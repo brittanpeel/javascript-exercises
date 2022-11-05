57ac7032068b0628362487feafeca59644fb8f33
Brittan Peel <92232598+brittanpeel@users.noreply.github.com>
2022-11-05 02:38:43 -0600

    completed challenge 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+    ftocConversion = ((temp - 32) * 5) / 9;
+    let ftocRounded = Math.round(ftocConversion * 10) / 10;
+    return ftocRounded;
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+    ctofConversion = (temp * 9) / 5 + 32;
+    let ctofRounded = Math.round(ctofConversion * 10) / 10;
+    return ctofRounded;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
