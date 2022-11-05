64cd46cea1755ee40284cb9b6d73db5cc96e529d
Brittan Peel <92232598+brittanpeel@users.noreply.github.com>
2022-11-05 02:14:56 -0600

    passed challenge 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+    let error = "ERROR";
+    let start = Math.min(args[0], args[1]);
+    let end = Math.max(args[0], args[1]);
+    let sum = 0;
 
+    if (args[0] < 0 || args[1] < 0) {
+        return error;
+    } else if (
+        isNaN(args[0]) ||
+        isNaN(args[1]) ||
+        typeof args[0] === "string" ||
+        typeof args[1] === "string"
+    ) {
+        return error;
+    } else {
+        for (let i = start; i <= end; i++) {
+            sum += i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
